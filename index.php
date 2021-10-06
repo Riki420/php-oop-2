6a240b6137823bb3bd4c3031a241969b3aec8a85
Riki420 <girottiriccardo1998@gmail.com>
2021-10-06 14:22:42 +0200

    Istanza User + User Premium

--- index.php
+++ index.php
@@ -3,3 +3,33 @@
 // Creo le classi necessarie per creare uno shop online
 //Strutturare le classi gestendo l'ereditarietà dove necessario; 
 //ad esempio ci potrebbero essere degli utenti premium che hanno diritto a degli sconti esclusivi
+
+
+//creo una classe utente
+class User
+{
+    public $firstname;
+    public $lastname;
+    public $email;
+}
+
+
+//creo una classe utente premiumù
+class UserPremium extends User
+{
+    public $point = 50;
+}
+
+//istanza user
+$user = new User();
+
+//istanza user premium
+$userPremium = new UserPremium();
+
+
+
+
+
+
+
+var_dump($user, $userPremium);
