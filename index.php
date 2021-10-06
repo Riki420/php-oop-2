d1bafac60612ccff8331a8097d15f3a903a03f66
Riki420 <girottiriccardo1998@gmail.com>
2021-10-06 15:08:23 +0200

    echo sui prodotti e utenti + istanza product

--- index.php
+++ index.php
@@ -11,22 +11,38 @@ class User
     public $firstname;
     public $lastname;
     public $email;
+
+
+    //getFullDetail
+    public function getFullDetail()
+    {
+        return $this->lastname . ' ' . $this->firstname . ' ' .  $this->email;
+    }
 }
 
 
 //Creo una classe utente premiumù
 class UserPremium extends User
 {
-    public $point = 50;
+    private $userPoint = 50;
+    //getUserPoint
+    public function getUserPoint()
+    {
+        return $this->userPoint;
+    }
 }
 
 //Istanza user
 $user = new User();
+$user->firstname = 'Marshall';
+$user->lastname = 'Reader';
+$user->email = 'marshder@gmail.com';
 
 //Istanza user premium
 $userPremium = new UserPremium();
-
-var_dump($user, $userPremium);
+$userPremium->firstname = 'Frank';
+$userPremium->lastname = 'Amadeus';
+$userPremium->email = 'frankdeus@gmail.com';
 
 //Creo classe Product
 class Product
@@ -34,9 +50,24 @@ class Product
     public $productName;
     public $price;
     public $specs;
+
+    //getProductDetail
+    public function getProductDetail()
+    {
+        return $this->productName . ' ' . $this->price . ' ' . $this->specs;
+    }
 }
 
 //Istanza Product
-$product = new Product();
+$speaker = new Product();
+$speaker->productName = 'Marshall Bluetooth Speaker';
+$speaker->price = 19.90;
+$speaker->specs = 'Bluetooth Speaker';
 
-var_dump($product);
+
+//echo per stampare in pagina ogni cosa
+echo '<h2>Informazioni Utenti</h2>';
+echo 'Utente non Premium: <br>' .  $user->getFullDetail();
+echo '<br>Utente Premium: <br>' . $userPremium->getFullDetail() . ' ' . $userPremium->getUserPoint();
+echo '<h2>Informazioni Prodotti</h2>';
+echo $speaker->getProductDetail();
