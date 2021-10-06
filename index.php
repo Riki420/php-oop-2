964817f982998ddedfbce40ba3a9314b3e7dfe3e
Riki420 <girottiriccardo1998@gmail.com>
2021-10-06 14:14:47 +0200

    Init

---
+++ index.php
@@ -0,0 +1,5 @@
+
+<?php
+// Creo le classi necessarie per creare uno shop online
+//Strutturare le classi gestendo l'ereditarietà dove necessario; 
+//ad esempio ci potrebbero essere degli utenti premium che hanno diritto a degli sconti esclusivi
